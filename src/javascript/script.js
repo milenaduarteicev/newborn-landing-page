e89a0e6a9136ec408c1d3d7a8ecc2114457689c2
milenaduarteicev <milena.duarte@somosicev.com>
2024-04-22 15:10:15 -0300

    Home responsivo

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    $('botao_mobile').on('click', function () {
-        $('menu_mobile').toggleClass('active')
+    $('#botao_mobile').on('click', function () {
+        $('#menu_mobile').toggleClass('ativo');
+        $('#botao_mobile').find('i').toggleClass('fa-x');
     });
+});
+
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('#header');
+    header.classList.toggle('rolagem', window.scrollY > 500);
 })
